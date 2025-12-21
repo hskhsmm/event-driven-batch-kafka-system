b4e7fd9c7acef516b1fc2834975325755af60cbe
hskhsmm <hskhsmm@naver.com>
2025-12-21 13:00:43 +0900

    feat: 캠페인 응답 DTO

---
+++ src/main/java/io/eventdriven/batchkafka/api/controller/dto/CampaignResponse.java
@@ -0,0 +1,22 @@
+package io.eventdriven.batchkafka.api.controller.dto;
+
+import io.eventdriven.batchkafka.domain.entity.Campaign;
+import io.eventdriven.batchkafka.domain.entity.CampaignStatus;
+import lombok.Getter;
+
+@Getter
+public class CampaignResponse {
+    private Long id;
+    private String name;
+    private Long totalStock;
+    private Long currentStock;
+    private CampaignStatus status;
+
+    public CampaignResponse(Campaign campaign) {
+        this.id = campaign.getId();
+        this.name = campaign.getName();
+        this.totalStock = campaign.getTotalStock();
+        this.currentStock = campaign.getCurrentStock();
+        this.status = campaign.getStatus();
+    }
+}
