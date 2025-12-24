0890b3de5abb7fb34d7194979700a3928e4cda4f
hskhsmm <hskhsmm@naver.com>
2025-12-25 01:02:36 +0900

    refactor: 재고 차감 로직을 원자적 UPDATE로 개선 및 코드 정리

--- src/main/java/io/eventdriven/batchkafka/domain/repository/CampaignRepository.java
+++ src/main/java/io/eventdriven/batchkafka/domain/repository/CampaignRepository.java
@@ -2,6 +2,22 @@ package io.eventdriven.batchkafka.domain.repository;
 
 import io.eventdriven.batchkafka.domain.entity.Campaign;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CampaignRepository extends JpaRepository<Campaign, Long> {
+
+    /**
+     * 원자적 재고 차감
+     *
+     * WHERE 조건으로 재고 검증 + SET으로 차감을 하나의 SQL로 실행
+     *
+     * @param id 캠페인 ID
+     * @return 업데이트된 행의 개수 (0: 재고 부족, 1: 성공)
+     */
+    @Modifying
+    @Query("UPDATE Campaign c SET c.currentStock = c.currentStock - 1 " +
+           "WHERE c.id = :id AND c.currentStock > 0")
+    int decreaseStockAtomic(@Param("id") Long id);
 }
