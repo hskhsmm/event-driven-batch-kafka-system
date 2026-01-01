a6841d1f7b40f5c7da30c6eca43599fbe3790b10
hskhsmm <hskhsmm@naver.com>
2026-01-02 02:14:50 +0900

    feat: ParticipationEvent에 Kafka 메타데이터 필드 추가

--- src/main/java/io/eventdriven/batchkafka/application/event/ParticipationEvent.java
+++ src/main/java/io/eventdriven/batchkafka/application/event/ParticipationEvent.java
@@ -4,6 +4,11 @@ public class ParticipationEvent {
     private Long campaignId;
     private Long userId;
 
+    // Kafka 메타데이터 (Consumer에서 설정)
+    private Long kafkaOffset;
+    private Integer kafkaPartition;
+    private Long kafkaTimestamp;
+
     public ParticipationEvent() {
     }
 
@@ -18,5 +23,29 @@ public class ParticipationEvent {
 
     public Long getUserId() {
         return userId;
+    }
+
+    public Long getKafkaOffset() {
+        return kafkaOffset;
+    }
+
+    public void setKafkaOffset(Long kafkaOffset) {
+        this.kafkaOffset = kafkaOffset;
+    }
+
+    public Integer getKafkaPartition() {
+        return kafkaPartition;
+    }
+
+    public void setKafkaPartition(Integer kafkaPartition) {
+        this.kafkaPartition = kafkaPartition;
+    }
+
+    public Long getKafkaTimestamp() {
+        return kafkaTimestamp;
+    }
+
+    public void setKafkaTimestamp(Long kafkaTimestamp) {
+        this.kafkaTimestamp = kafkaTimestamp;
     }
 }
